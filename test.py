0897c18d89075bda8c6ab83a879b8301ae2eb911
khari <khari@Luz.Owl_House>
2023-03-28 11:25:20 -0400

    Messing with test.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ router_mac = send[ARP].hwsrc
 router_ip = send[ARP].psrc
 print(f"Program took {time.time() - x} seconds to get response for ARP message.")
 
-ping_pkt = Ether(dst=router_mac)/ICMP()/b"Message Confirmation"
+ping_pkt = Ether(dst=router_mac)/IP(dst=router_ip)/ICMP()/b"Message Confirmation"
 ping_pkt.show()
 send1 = sr1(ping_pkt)
 if send1 is not None:
