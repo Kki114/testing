b93737d09fd1a832ba266bd7f54e611c53f8451a
khari <khari@Luz.Owl_House>
2024-07-31 21:07:22 -0400

    Troubleshooting why the code won't send the packet.

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 # Testing some scapy functionality
 
-from scapy.all import sr1, Ether, IP, ICMP, ARP
+from scapy.all import srp1, sr1, Ether, IP, ICMP, ARP
 import time
 
 x = time.time()
-pkt = ARP(op="who-has", pdst="192.168.1.1", hwdst="ff:ff:ff:ff:ff:ff")
-pkt.show()
+pkt = Ether()/ARP(op="who-has", pdst="192.168.1.1", hwlen=6, plen=4, psrc="192.168.1.102", hwdst="ff:ff:ff:ff:ff:ff")
+#pkt.show()
+# Copies broadcast address into "broadcast" variable
 broadcast = pkt[ARP].hwdst
+#print(broadcast)
 
-# Next line breaks code at 'sr1'
-send = sr1(pkt)
+# Next line breaks code when attempting to send packet
+ARP.show()
+send = sr1(ARP)
+send.show()
 
 router_mac = send[ARP].hwsrc
 router_ip = send[ARP].psrc
